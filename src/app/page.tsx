eb4adfe2f45d4494523008ddf037858562f3c6a8
NandoLara07 <nandolaradossantos@gmail.com>
2025-09-13 01:35:46 -0300

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,103 +1,149 @@
 import Image from "next/image";
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
 
 export default function Home() {
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+    <main className="flex justify-center bg-gray-100 min-h-screen p-4 pt-5 pb-5">
+      <div className="bg-white w-full max-w-[794px] shadow-lg rounded-lg px-10 py-20">
+        <h1 className="text-4xl mb-10 text-center font-oswald">
+          Diário de bordo - Job Interview Kombat
+        </h1>
+        <h2 className="font-oswald text-2xl mb-6 underline">
+          Desenvolvimento do projeto
+        </h2>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+        <Accordion type="multiple" className="w-full" defaultValue={["item-1"]}>
+          <AccordionItem value={"item-1"}>
+            <AccordionTrigger className="font-oswald font-normal text-2xl text-black">
+              Semana 1 (27/07/2025 - 02/08/2025):{" "}
+            </AccordionTrigger>
+
+            <AccordionContent className="font-arial text-base text-black">
+              <ul className="list-disc list-inside ml-4">
+                <li>
+                  Na primeira semana, após criarmos o repositório no Github do
+                  projeto, focamos em criar a identidade visual do site. Por ser
+                  um RPG que apresenta uma “luta verbal” simulando os jogos de
+                  luta, nos inspiramos bastante no jogo de videogame “Mortal
+                  Kombat”.
+                  <p className="my-4">
+                    Foi assim que tivemos a primeira impressão de como o projeto
+                    ficaria.
+                  </p>
+                  <Image
+                    src="/image01.png"
+                    alt="Imagem do design inicial do projeto"
+                    width={600}
+                    height={600}
+                    className="mx-auto rounded-sm"
+                  />
+                  <br />
+                </li>
+
+                <li>
+                  Com os botões já posicionados, decidimos fazer o fundo.
+                  <Image
+                    src="/image02.png"
+                    alt="Imagem do design inicial do projeto"
+                    width={600}
+                    height={600}
+                    className="mx-auto my-4 rounded-sm"
+                  />
+                </li>
+
+                <li>
+                  Criamos o protótipo de como seria o layout da área de regras
+                  <Image
+                    src="/image03.png"
+                    alt="Imagem do design inicial do projeto"
+                    width={600}
+                    height={600}
+                    className="mx-auto my-4 rounded-sm"
+                  />
+                </li>
+
+                <li>
+                  Adicionamos um som ao passar o mouse pelas opções do Menu
+                  <div className="w-full max-w-[600px] mx-auto aspect-video my-4">
+                    <iframe
+                      src="https://www.youtube.com/embed/LSPjR5Lgi-8?si=rbHw-iLgVA70YCo6"
+                      title="YouTube video player"
+                      className="w-full h-full rounded-sm shadow-lg"
+                      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                      allowFullScreen
+                    ></iframe>
+                  </div>
+                </li>
+              </ul>
+            </AccordionContent>
+          </AccordionItem>
+
+          <AccordionItem value={"item-2"}>
+            <AccordionTrigger className="font-oswald font-normal text-2xl text-black">
+              Semana 2 (03/08/2025 - 09/08/2025):{" "}
+            </AccordionTrigger>
+
+            <AccordionContent className="font-arial text-base text-black">
+              <p className="font-arial">Conteúdo da semana 2</p>
+            </AccordionContent>
+          </AccordionItem>
+
+          <AccordionItem value={"item-3"}>
+            <AccordionTrigger className="font-oswald font-normal text-2xl text-black">
+              Semana 3 (10/08/2025 - 16/08/2025):{" "}
+            </AccordionTrigger>
+
+            <AccordionContent className="font-arial text-base text-black">
+              <p className="font-arial">Conteúdo da semana 3</p>
+            </AccordionContent>
+          </AccordionItem>
+
+          <AccordionItem value={"item-4"}>
+            <AccordionTrigger className="font-oswald font-normal text-2xl text-black">
+              Semana 4 (17/08/2025 - 23/08/2025):{" "}
+            </AccordionTrigger>
+
+            <AccordionContent className="font-arial text-base text-black">
+              <p className="font-arial">Conteúdo da semana 4</p>
+            </AccordionContent>
+          </AccordionItem>
+
+          <AccordionItem value={"item-5"}>
+            <AccordionTrigger className="font-oswald font-normal text-2xl text-black">
+              Semana 5 (24/08/2025 - 30/08/2025):{" "}
+            </AccordionTrigger>
+
+            <AccordionContent className="font-arial text-base text-black">
+              <p className="font-arial">Conteúdo da semana 5</p>
+            </AccordionContent>
+          </AccordionItem>
+
+          <AccordionItem value={"item-6"}>
+            <AccordionTrigger className="font-oswald font-normal text-2xl text-black">
+              Semana 6 (31/08/2025 - 06/09/2025):{" "}
+            </AccordionTrigger>
+
+            <AccordionContent className="font-arial text-base text-black">
+              <p className="font-arial">Conteúdo da semana 6</p>
+            </AccordionContent>
+          </AccordionItem>
+
+          <AccordionItem value={"item-7"}>
+            <AccordionTrigger className="font-oswald font-normal text-2xl text-black">
+              Semana 7 (07/09/2025 - 13/09/2025):{" "}
+            </AccordionTrigger>
+
+            <AccordionContent className="font-arial text-base text-black">
+              <p className="font-arial">Conteúdo da semana 7</p>
+            </AccordionContent>
+          </AccordionItem>
+        </Accordion>
+      </div>
+    </main>
   );
 }
